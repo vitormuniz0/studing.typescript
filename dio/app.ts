56ffbe962817bb770a94792ab3144e84ea8a067d
vitormuniz0 <vitormuniz311@gmail.com>
2024-11-19 22:23:07 -0300

    Aprendendo mais sobre classes

--- dio/app.ts
+++ dio/app.ts
@@ -1,5 +1,25 @@
-const soma = (a: number, b: number) =>{
-    console.log(a + b)
+// Muniz Bank
+
+// name , accountBank
+// depositar , sacar
+
+class Account {
+  name: string;
+  accountNumber: number;
+
+  constructor(name: string, accountNumber: number) {
+    this.name = name;
+    this.accountNumber = accountNumber;
+  }
+
+  deposit = (name:string) =>{
+    console.log("Olá", name, "Voce depositou dinheiro");
+  }
+
+  withdraw = () =>{
+    console.log("Voce sacou")
+  }
 }
 
-soma(2,3);
+const newAccount: Account = new Account('Vitor', 2);
+newAccount.deposit("Vitor");
